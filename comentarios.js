d67930f78a0c52a106d02184da4376f1f1fd02e5
DanielNunesPT <131387962+DanielNunesPT@users.noreply.github.com>
2024-05-25 19:42:53 +0100

    Create comentarios.js

---
+++ comentarios.js
@@ -0,0 +1,152 @@
+[
+    {
+        "nome_do_restaurante": "Restaurante Mauritânia",
+        "localizacao_do_restaurante": "Rua João Maia, 124, Maia",
+        "rating": 4.5,
+        "tipo_de_gastronomia": "Peixes e Mariscos",
+        "comentarios": [
+            {
+                "autor": "João Silva",
+                "data": "2024-01-15",
+                "comentario": "Excelente comida e atendimento. Recomendo os mariscos."
+            },
+            {
+                "autor": "Maria Oliveira",
+                "data": "2024-02-20",
+                "comentario": "Ambiente agradável, mas o serviço pode ser um pouco lento."
+            }
+        ]
+    },
+    {
+        "nome_do_restaurante": "Restaurante O Barrote",
+        "localizacao_do_restaurante": "Rua da Constituição, 255, Maia",
+        "rating": 4.3,
+        "tipo_de_gastronomia": "Portuguesa",
+        "comentarios": [
+            {
+                "autor": "Carlos Ferreira",
+                "data": "2024-03-10",
+                "comentario": "Comida típica portuguesa muito saborosa."
+            },
+            {
+                "autor": "Ana Gomes",
+                "data": "2024-04-05",
+                "comentario": "Preço justo e boa qualidade. Voltarei com certeza."
+            }
+        ]
+    },
+    {
+        "nome_do_restaurante": "Cantina 32",
+        "localizacao_do_restaurante": "Avenida Visconde Barreiros, 421, Maia",
+        "rating": 4.4,
+        "tipo_de_gastronomia": "Contemporânea",
+        "comentarios": [
+            {
+                "autor": "Ricardo Almeida",
+                "data": "2024-02-28",
+                "comentario": "Ambiente moderno e pratos bem elaborados."
+            }
+        ]
+    },
+    {
+        "nome_do_restaurante": "Restaurante Astória",
+        "localizacao_do_restaurante": "Praça Dr. José Vieira de Carvalho, 16, Maia",
+        "rating": 4.2,
+        "tipo_de_gastronomia": "Internacional",
+        "comentarios": [
+            {
+                "autor": "Sofia Costa",
+                "data": "2024-01-30",
+                "comentario": "Variedade de pratos internacionais, gostei bastante."
+            }
+        ]
+    },
+    {
+        "nome_do_restaurante": "Restaurante Adega da Maia",
+        "localizacao_do_restaurante": "Rua Augusto Simões, 155, Maia",
+        "rating": 4.6,
+        "tipo_de_gastronomia": "Portuguesa Tradicional",
+        "comentarios": [
+            {
+                "autor": "Pedro Sousa",
+                "data": "2024-03-12",
+                "comentario": "A verdadeira comida portuguesa, feita com amor."
+            },
+            {
+                "autor": "Mariana Ribeiro",
+                "data": "2024-04-18",
+                "comentario": "Adorei a decoração e o atendimento personalizado."
+            }
+        ]
+    },
+    {
+        "nome_do_restaurante": "Restaurante Os Loureiros",
+        "localizacao_do_restaurante": "Rua Central da Maia, 545, Maia",
+        "rating": 4.7,
+        "tipo_de_gastronomia": "Portuguesa",
+        "comentarios": [
+            {
+                "autor": "Rui Martins",
+                "data": "2024-05-05",
+                "comentario": "Um dos melhores restaurantes da Maia, sem dúvida."
+            },
+            {
+                "autor": "Isabel Fernandes",
+                "data": "2024-05-20",
+                "comentario": "A comida é excelente e o serviço impecável."
+            }
+        ]
+    },
+    {
+        "nome_do_restaurante": "Restaurante A Cozinha da Maria",
+        "localizacao_do_restaurante": "Rua António Feliciano de Castilho, 42, Maia",
+        "rating": 4.4,
+        "tipo_de_gastronomia": "Caseira",
+        "comentarios": [
+            {
+                "autor": "José Mendes",
+                "data": "2024-01-25",
+                "comentario": "Comida caseira de qualidade, me senti em casa."
+            }
+        ]
+    },
+    {
+        "nome_do_restaurante": "Restaurante O Forno",
+        "localizacao_do_restaurante": "Rua do Chantre, 12, Maia",
+        "rating": 4.3,
+        "tipo_de_gastronomia": "Italiana",
+        "comentarios": [
+            {
+                "autor": "Laura Nunes",
+                "data": "2024-02-10",
+                "comentario": "Melhor pizza que já comi! Ambiente agradável."
+            }
+        ]
+    },
+    {
+        "nome_do_restaurante": "Restaurante Tappas Caffé",
+        "localizacao_do_restaurante": "Rua Padre António, 65, Maia",
+        "rating": 4.5,
+        "tipo_de_gastronomia": "Mediterrânea",
+        "comentarios": [
+            {
+                "autor": "Tiago Pinto",
+                "data": "2024-03-15",
+                "comentario": "Tapas deliciosas e ótimo atendimento."
+            }
+        ]
+    },
+    {
+        "nome_do_restaurante": "Restaurante Casa da Avó",
+        "localizacao_do_restaurante": "Rua Padre António, 98, Maia",
+        "rating": 4.2,
+        "tipo_de_gastronomia": "Portuguesa",
+        "comentarios": [
+            {
+                "autor": "Vera Cardoso",
+                "data": "2024-04-08",
+                "comentario": "Comida com sabor de casa da avó. Recomendo."
+            }
+        ]
+    }
+]
